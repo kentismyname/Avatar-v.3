5bf32ddb0c7629d7f2de7039c98f77331afc6f48
John Kenneth Dalisay <>
2024-08-07 15:49:44 +0800

    added dark and light mode feature

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggle = document.getElementById('toggle');
+    const body = document.body;
+
+    toggle.addEventListener('change', function() {
+        if (toggle.checked) {
+            body.classList.add('dark-mode');
+        } else {
+            body.classList.remove('dark-mode');
+        }
+    });
+});
